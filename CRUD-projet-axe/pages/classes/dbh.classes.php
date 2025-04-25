c956053e42e13bce94b640f4940443d88be6c3ec
Nicolas-frnc <174860069+Nicolas-frnc@users.noreply.github.com>
2025-04-25 22:47:17 +0200

    Add user signup functionality with some validation checks

---
+++ CRUD-projet-axe/pages/classes/dbh.classes.php
@@ -0,0 +1,26 @@
+<?php
+
+class Dbh {
+
+    protected function connect() {
+        try {
+            $username = "root";
+            $password = "root";
+            $dbh = new PDO('mysql:host=localhost;dbname=musicard', $username, $password );
+
+        }
+        catch (PDOException $e) { // https://youtu.be/BaEm2Qv14oU?list=LL&t=1608
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ CRUD-projet-axe/pages/classes/signup.contr.php
@@ -0,0 +1,95 @@
+<?php
+
+class SignupContr {
+
+    private $uid;
+    private $pwd;
+    private $pwdRepeat;
+    private $email;
+    public function __construct($uid, $pwd, $pwdRepeat, $email) {
+        $this->$uid = $uid;
+        $this->$pwd = $pwd;
+        $this->$pwdRepeat = $pwdRepeat;
+        $this->$email = $email;
+    }
+
+
+    private function signupUser(){
+        if($this->emptyInput() == false){
+            header("location: ../register.php?error=emptyinput"); // https://youtu.be/BaEm2Qv14oU?t=2584
+        }
+        if($this->invalidUid() == false){
+            header("location: ../register.php?error=usurname");
+        }
+        if($this->invalidEmail() == false){
+            header("location: ../register.php?error=email");
+        }
+        if($this->pwdMatch() == false){
+            header("location: ../register.php?error=passwordmatch");
+        }
+        if($this->uidTakenCheck() == false){
+            header("location: ../register.php?error=useroremailtaken");
+        }
+
+        $this->setUser();
+
+    }
+
+    private function emptyInput(){
+        $result;
+        if(empty($this->uid) || empty($this->pwd) || empty($this->pwdRepeat) || empty($this->email)){
+            $result = false;
+
+        }
+        else{
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidUid(){
+        $result;
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)) { // https://youtu.be/BaEm2Qv14oU?list=LL&t=1376
+            $result = false;
+        }
+        else{
+            $result = true;
+        }
+        return $result;
+    }
+    private function invalidEmail(){
+        $result;
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $result = false;
+        }
+        else{
+            $result = true;
+        }
+    }
+    private function pwdMatch(){
+        $result;
+        if ($this->pwd !== $this->pwdRepeat) {
+            $result = true;
+        }
+        else{
+            $result = false;
+        }
+        return $result;
+    }
+
+    private function uidTakenCheck(){
+        $result;
+        if (!$this->checkUser($this->$uid, $this->$email)) {
+            $result = true;
+        }
+        else{
+            $result = false;
+        }
+        return $result;
+    }
+
+
+
+
+
+}
